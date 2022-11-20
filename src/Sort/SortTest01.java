3ea33afb315fbf68802bac83e20f00f0c81bef4e
MasonJJS <106791809+MasonJJS@users.noreply.github.com>
2022-11-20 20:51:17 +0900

    today study

---
+++ src/Sort/SortTest01.java
@@ -0,0 +1,50 @@
+package Sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortTest01 {
+
+    public int[] solution(int[] arr) {
+
+        //[4,3,2,1]	[4,3,2]
+        //[10]	[-1], Collections.reverseOrder());
+        if(arr.length == 1){
+
+            return new int[]{-1};
+        }
+        List<Integer> arrToList = Arrays.stream(arr)
+                                        .boxed()
+                                        .collect(Collectors.toList());
+        arrToList.sort(Comparator.reverseOrder());
+        arrToList.remove(arrToList.size()-1);
+
+        int[] answer = arrToList.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+    public int[] otherSolution(int[] arr){
+//        int min = Integer.MAX_VALUE;
+//        if(arr.length == 1){
+//            return new int[] {-1};
+//        }
+//        for(int a : arr) if(a<min) min = a;
+//        int index = 0;
+//        int[] answer = new int[arr.length-1];
+//        for(int a : arr){
+//            if (a == min ) continue;
+//            answer[index++] = a;
+//        }
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(i->i!=min).toArray();
+//        return answer;
+    }
+
+    public static void main(String[] args) {
+        SortTest01 s = new SortTest01();
+        s.solution(new int[]{ 32,123,21,332,3122,41
+        });
+    }
+
+}
---
+++ src/Sort/SortTest04.java
@@ -0,0 +1,22 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class SortTest04 {
+    public int solution(int[] citations) {
+        //[0,1,3,5,6]
+        Arrays.sort(citations);
+        int h = 0;
+        for(int i = 0; i<citations.length;i++){
+            h = citations.length - i;
+            if(citations[i] >= h) return h;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        SortTest04 s = new SortTest04();
+
+        int a = s.solution(new int[]{0,3,1,5,6});
+        System.out.println(a);
+    }
+}
